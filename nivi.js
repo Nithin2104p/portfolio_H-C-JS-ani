ca77cab8793c851449394573f78103f96b92babd
Nithin2104p <tnithin688@gmail.com>
2024-03-23 20:13:54 +0530

    ready to deploy

---
+++ nivi.js
@@ -0,0 +1,24 @@
+
+ScrollReveal({
+    reset: true,
+    distance: '100px',
+    duration: 1000,
+    delay: 2000
+});
+ScrollReveal().reveal('.ptf', { delay: 100, origin: 'bottom' })
+ScrollReveal().reveal('.navbar', { delay: 100, origin: 'left' })
+ScrollReveal().reveal('#move', { delay: 0, origin: 'top' })
+ScrollReveal().reveal('.home', { delay: 100, origin: 'top' })
+ScrollReveal().reveal('.nithin', { delay: 100, origin: 'bottom' })
+ScrollReveal().reveal('#zoro', { delay: 100, origin: 'top' })
+
+
+ScrollReveal().reveal('.services', { delay: 100, origin: 'bottom' })
+
+
+ScrollReveal().reveal('#kuma', { delay: 100, origin: 'left' })
+ScrollReveal().reveal('#kuma1', { delay: 100, origin: 'right' })
+ScrollReveal().reveal('#kuma2', { delay: 100, origin: 'left' })
+ScrollReveal().reveal('#kuma3', { delay: 100, origin: 'right' })
+
+ScrollReveal().reveal('.zimbei', { delay: 100, origin: 'right' })
